fd99b435e79b89dbcf8402f948a2b030edf1259e
awaado <56788580+ahmedawaad1804@users.noreply.github.com>
2020-01-26 12:41:03 +0200

    second commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Kacity
+# U-Control
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.15.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kacity'`, () => {
+  it(`should have as title 'U-Control'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('kacity');
+    expect(app.title).toEqual('U-Control');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('kacity app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('U-Control app is running!');
   });
 });
